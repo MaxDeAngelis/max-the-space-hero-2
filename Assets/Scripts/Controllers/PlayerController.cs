49621690c801d7beed377997f743a12b865614f4
Maxwell DeAngelis <maxwell.deangelis@gmail.com>
2015-06-24 22:10:14 -0400

    Updated Max to have an arm so I could get the rotation logic to work for aiming

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -39,22 +39,21 @@ public class PlayerController : MonoBehaviour {
 	 * @private Called 60times per second fixed, handles all processing
 	 **/
 	void FixedUpdate() {
-		// If attacking fire the attack animation
+		// Line cast to the ground check transform to see if it is over a ground layer to prevent double jump
+		_isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		
+		// Set the default vertical velocity to what it currently is
+		float _verticalVelocity = _rigidbody.velocity.y;
+
+		/* ---- HANDLE FIRING GUN ---- */
 		if (Input.GetButtonDown("Fire1")) {
 			Vector3 pos = Input.mousePosition;
 			pos.z = transform.position.z - Camera.main.transform.position.z;
 			pos = Camera.main.ScreenToWorldPoint(pos);
-
 			_weapon.fire(pos);
 		}
 
-		// Line cast to the ground check transform to see if it is over a ground layer to prevent double jump
-		_isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-
-		// Set the default vertical velocity to what it currently is
-		float _verticalVelocity = _rigidbody.velocity.y;
-
-		// If over some climbable object re-calculate the vertical velocity based on the climbable objects speed up/down
+		/* ---- HANDLE IF OVER A CLIMBABLE OBJECT ---- */
 		if (_climbableController) {
 			if (Input.GetAxis("Vertical") > 0) {
 				_verticalVelocity = _climbableController.upSpeed;
@@ -65,11 +64,14 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 
+		/* ---- HANDLE MOVING HORIZONTALLY AND VERTICALLY ---- */
 		// See what the horizontal axis is to know if moving left or right
 		float horizontalAxis = Input.GetAxis("Horizontal");
 		_rigidbody.velocity = new Vector2(horizontalAxis * maxSpeed, _verticalVelocity);
 
 		// Handle flipping the transform depending on the direction you are heading
+		// TODO: Need to figure out how to handle flipping for gun and movement. Right now they are kinda conflicting
+		// as the player aims oposite of movement it is flipping twice per frame
 		if (horizontalAxis > 0 && !facingRight) {
 			_flipPlayer();
 		} else if (horizontalAxis < 0 && facingRight) {
@@ -80,6 +82,31 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetButtonDown("Jump") && _isGrounded) {
 			_rigidbody.AddForce(new Vector2(0f, jumpForce));
 		}
+
+		/* ---- AIM THE ARM TO FIRE ----*/
+		// Get a reference the the game object of the arm
+		GameObject arm = _weapon.transform.parent.gameObject;
+
+		// Get mouse position and arm position
+		Vector2 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		Vector3 armPos = Camera.main.WorldToViewportPoint (arm.transform.position);
+
+		// Get arm and mouse position relative to the game object
+		Vector2 relativeArmPos = new Vector2(armPos.x - 0.5f, armPos.y - 0.5f);
+		Vector2 relativeMousePos = new Vector2 (mousePos.x - 0.5f, mousePos.y - 0.5f) - relativeArmPos;
+
+		// Calculate the angle
+		float angle = Vector2.Angle (Vector2.down, relativeMousePos);
+
+		// Flip the player if aiming in the opposite direction
+		if ((relativeMousePos.x < 0 && facingRight) || (relativeMousePos.x > 0 && !facingRight)) {
+			_flipPlayer();
+		}
+
+		// Calculate the Quaternion and rotate the arm
+		Quaternion quat = Quaternion.identity;
+		quat.eulerAngles = new Vector3(0, 0, angle);
+		arm.transform.rotation = quat;
 	}
 
 	/**
