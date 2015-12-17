b0bc254b8292a4475be3d02f90b2e12bb1a5768f
Maxwell DeAngelis <maxwell.deangelis@gmail.com>
2015-12-17 00:52:31 -0500

    Updated platforms to use full sprites instead of tiles because of crappy tile display. Might need to re think in the long run but for now full play forms have been added for each length

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -13,6 +13,9 @@ public class CameraManager : MonoBehaviour {
 	public GameObject topBoundry;				// Top boundry game object
 	public GameObject bottomBoundry;			// Bottom boundry game object
 
+	public Camera miniMap;
+	public GameObject miniMapBorder;
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	/// 								     		PRIVATE VARIABLES											     ///
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -41,6 +44,17 @@ public class CameraManager : MonoBehaviour {
 	 * @private Called once per frame after all updates and fixed updates finish
 	 **/
 	void LateUpdate () {
+
+		/*if (miniMap) {
+			Vector3 test = _camera.WorldToScreenPoint(miniMapBorder.transform.position);
+
+
+			Rect newRect = new Rect((test.x / _cameraWidth) / 100, (test.y / _cameraHeight) / 100, 0.15f, 0.15f);
+
+			miniMap.rect = newRect;
+
+		}*/
+
 		/* TODO: Commented out the target mouse logic because as it targets is keeps following mouse
 		 * seems a bit weird but didnt want to loose code so leaving disabled for now
 		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) {
